740a34548282c0be6a8609455ff217c7d06acf08
Lee Crossley <leee@hotmail.co.uk>
2015-01-31 11:58:01 +0000

    Bootstrap and handle errors

--- app/main.js
+++ app/main.js
@@ -1,4 +1,10 @@
 var app = require("application");
 
 app.mainModule = "app/views/list-view";
+
+app.onUncaughtError = function (err) {
+    console.log("Application error: " + err.name +
+        "; " + err.message + "; " + err.nativeError);
+};
+
 app.start();
